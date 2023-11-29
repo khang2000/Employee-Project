6106702473218f55483c86da3c6cfb905e3204e5
khang2000 <khanghtqt70@gmail.com>
2023-11-29 16:13:20 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+app.use("/", (req, res) => {
+  res.send("server is running");
+});
+app.listen(5000, console.log("server on Port 5000"));
